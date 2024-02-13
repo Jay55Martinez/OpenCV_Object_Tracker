80185b928453f137c1e5cee0707e3f9d93f15c49
Jay55Martinez <martinez.jay@northeastern.edu>
2024-02-13 18:09:02 -0500

    Detoector class reginition method added

--- src/detector.py
+++ src/detector.py
@@ -1,33 +1,33 @@
 # Detector.py is responsible for object detection and recognition. At the moment
 # there will be 3 modes of detection:
 
-# - [ ] recognition tracking: detector will be trained on images of an person and track only them
-# - [ ] motion tracking: detector will track anything moving in the frame
-# - [ ] face tracking: detector will track faces
+# - [x] recognition tracking: detector will be trained on images of an person and track only them
+#   - ( ) create an iml file for traking yourself
+# - [ ] motion tracking: detector will track anything moving in the frame TODO not sure how to acomplish if the frame
+#       is also going to be moving
+# - [x] face tracking: detector will track faces
 
-# Detector Class
-
-# Design choice 
-#  - have Detector start the video or have frames be passed into it?
-#  
-#  - 
 import cv2 as cv
+import os
 from enum import Enum
 
 # Need an enum for mode 
 class Mode(Enum):
     FACE = 1
-    MOTION = 2   
+    MOTION = 2
+    FILE = 3   
+    
+# Detector Class
 
 class Detector:
     """
     Detector() 
     """
-    def __init__(self, mode, xml_file=None):
+    def __init__(self, mode, xml_file_path=None):
         self.ret = None
         self.mode = mode
         self.frame = None
-        self.ixl_file = xml_file
+        self.ixl_file = xml_file_path
         self.bounds = None
         
     # get the next frame to be processed returns box corrds (x, y, w, h)
@@ -39,28 +39,35 @@ class Detector:
             self.face_dect()
         elif self.mode == Mode.MOTION:
             self.motion_dect()
+        elif self.mode == Mode.FILE:
+            self.reginition_dect()
         else:
             raise RuntimeError("Detector mode not recognized")
         
     def reginition_dect(self):
-        pass
+        if os.path.exists(self.ixl_file):
+            haar_cascade = cv.CascadeClassifier(self.ixl_file)
+                
+        
+            # convert image to gray scale
+            gray = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
+            
+            aces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
+            
+            # face detected get the largest could change to getting the most acurate one
+            self.bounds = None
+            for (x, y, w, h) in aces_rect:
+                if self.bounds == None:
+                    self.bounds = (x, y, w, h)
+                elif (w * h) > self.bounds[2] * self.bounds[3]:
+                    self.bounds = (x, y, w, h)
+        else:
+            raise RuntimeError("xml file not found")
 
     def face_dect(self):
-        haar_cascade = cv.CascadeClassifier("../out/haar_face.xml")
-        
-        # convert image to gray scale
-        gray = cv.cvtColor(self.frame, cv.COLOR_BGR2GRAY)
-        
-        aces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3)
-        
-        # face detected get the largest could change to getting the most acurate one
-        self.bounds = None
-        for (x, y, w, h) in aces_rect:
-            if self.bounds == None:
-                self.bounds = (x, y, w, h)
-            elif (w * h) > self.bounds[2] * self.bounds[3]:
-                self.bounds = (x, y, w, h)
-                
+        self.ixl_file = "../out/haar_face.xml"
+        self.reginition_dect()        
     
+    # If the camera is alread moving then this will be hard to implement
     def motion_dect(self):
         pass
--- src/tracker.py
+++ src/tracker.py
@@ -1,3 +1,43 @@
 # tracker.py will draw bounding boxs around the tracked object and computing
-# the region fouce.
+# how many pixels (units) the object is displaced from the center of the screen
 
+# Class Tracker
+# - [ ] update method: takes in bounds of detected object from detector 
+# - [ ] draw method: draws the bounding box onto the frame
+# - [ ] get_displacement: ...
+
+class Tracker:
+    """Tracker()"""
+    def __init__(self, frame_width=None, frame_height=None, draw_box=False):
+        self.frame_width = frame_width
+        self.frame_height = frame_height
+        self.draw_box = draw_box
+        self.box_x = None
+        self.box_y = None
+        self.box_width = 1
+        self.box_height = 1
+        self.displacement_filter = None
+        self.displacement = 0
+    
+    def update(self, box_corrds):
+        if box_corrds != None:
+            self.box_x = box_corrds[0]
+            self.box_y = box_corrds[1]
+            self.box_width = box_corrds[2]
+            self.box_height = box_corrds[3]
+            
+            self.displacement = self.find_displacement()
+        else:
+            self.displacement = 0
+            
+    # left of the center of the screen will have (-) displacement
+    # right of the center of the screen will have (+) displacement
+    def find_displacement(self):
+        center = self.frame_width/2
+        
+        if self.box_x < center:
+            pass
+            
+        
+        
+        
