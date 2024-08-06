2eeca1686bfc562bd59746f0d3fa09e1c22f5a53
Jay55Martinez <jay55martinez@gmail.com>
2024-08-06 18:56:06 +0100

    Servo Util class - set_angle method

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from picamera2 import Picamera2
 import cv2 as cv
+import RPi.GPIO as GPIO
+import time
+from servo import ServoUtil
 from detector import Detector, Mode
 from tracker import Tracker2D
 
@@ -7,7 +10,19 @@ camera = Picamera2()
 camera.configure(camera.create_preview_configuration(main={'format':'XRGB8888', 'size':(3280,2464)}))
 camera.start()
 
-detector = Detector(Mode.FACE)
+# A0090
+motor = ServoUtil(11, 50, 2.5, 7.5, 12)
+print('here')
+time.sleep(2)
+motor.set_angle(0) # left -90 degrees 
+print('here')
+time.sleep(2)
+motor.set_angle(90) # center 
+print('here')
+time.sleep(2)
+motor.set_angle(180) # right 90 degrees
+
+detector = Detector(Mode.RAW)
 tracker = Tracker2D(detector)
 
 while True:
@@ -23,3 +38,5 @@ while True:
         break
     
 cv.destroyAllWindows()
+motor.finish()
+GPIO.cleanup()
---
+++ src/servo.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+
+'''
+Micro Servo 9g A0090:
+2.5 - left -90 degrees
+7.5 - center 0 degrees
+12 - right 90 degrees
+'''
+
+class ServoUtil:
+    '''
+    ServoUtil(GPIO_Pin, Frequency, Left, Center, Right) 
+
+    - Allows for the set-up of a servo and handling angle control.
+    - Left, Center, Right used to convert angle in to Duty Cycle
+    '''
+    def __init__(self, gpio_pin, frequency, left, center, right):
+        self.left = left
+        self.center = center
+        self.right = right
+
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(gpio_pin, GPIO.OUT)
+        
+        self.servo = GPIO.PWM(gpio_pin, frequency)
+        self.servo.start(0)
+    
+    def set_angle(self, angle):
+        '''
+        Changes the oritation of the servo based on the given center.
+        '''
+        duty_cycle = (angle * (self.right - self.left)) / 180 + self.left
+        if duty_cycle < self.left:
+            duty_cycle = self.left
+        elif duty_cycle > self.right:
+            duty_cycle = self.right 
+        self.servo.ChangeDutyCycle(duty_cycle)
+
+    def finish(self):
+        '''
+        Closes comunication with the servo.
+        '''
+        self.servo.stop()
